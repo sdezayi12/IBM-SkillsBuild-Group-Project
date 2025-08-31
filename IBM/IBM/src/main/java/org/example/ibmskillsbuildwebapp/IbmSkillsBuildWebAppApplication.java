82217884e7366d8ad86ad73f528db6cf7a758a83
Sarah Dezayi <sarahdezayi@Sarahs-Air-2.powerhub>
2025-08-31 15:38:04 +0100

    Update IBM folder

---
+++ IBM/IBM/src/main/java/org/example/ibmskillsbuildwebapp/IbmSkillsBuildWebAppApplication.java
@@ -0,0 +1,14 @@
+package org.example.ibmskillsbuildwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class IbmSkillsBuildWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmSkillsBuildWebAppApplication.class, args);
+	}
+
+}
